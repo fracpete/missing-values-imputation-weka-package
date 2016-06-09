4ae1d677a8e96950a0b7a96166e46bf593cdddb3
Peter Reutemann <fracpete@gmail.com>
2016-06-10 11:11:07 +1200

    fixed copyright

--- src/main/java/weka/filters/unsupervised/attribute/missingvaluesimputation/NullImputation.java
+++ src/main/java/weka/filters/unsupervised/attribute/missingvaluesimputation/NullImputation.java
@@ -15,7 +15,7 @@
 
 /*
  *    NullImputation.java
- *    Copyright (C) 2014 University of Waikato, Hamilton, New Zealand
+ *    Copyright (C) 2014-2016 University of Waikato, Hamilton, New Zealand
  *
  */
 
